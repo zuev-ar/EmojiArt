b12eb11a3b448d99221fef6019f41a3b72e83f55
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-11-09 19:27:33 +0500

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Arkasha Zuev on 09.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
